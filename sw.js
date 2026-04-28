2d977083cb8b4162df8bd0575683cc001cb42196
chibakagami <chibakagami369@gmail.com>
2026-04-28 21:52:10 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'travel-v1';
+const FILES = ['./travel-app.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./travel-app.html')))
+  );
+});
